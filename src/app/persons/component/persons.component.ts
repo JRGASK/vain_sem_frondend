2e8ebb824d38dc63474df5e667f801b868e877fc
jurajgajdiciar <juraj.gajdiciar9@gmail.com>
2024-12-02 12:11:32 +0100

    init get personByEmail 2

--- src/app/persons/component/persons.component.ts
+++ src/app/persons/component/persons.component.ts
@@ -1,37 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, NgIf } from '@angular/common';
 import { LoginService } from '../../login/service/login.service';
 import { PersonsService } from '../service/persons.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-persons',
   standalone: true,
-  imports: [HttpClientModule,JsonPipe],
+  imports: [HttpClientModule, JsonPipe, NgIf, ReactiveFormsModule, FormsModule],
   providers: [LoginService, PersonsService],
   templateUrl: './persons.component.html',
-  styleUrl: './persons.component.css'
+  styleUrl: './persons.component.css',
 })
-export class PersonsComponent implements OnInit{
-
+export class PersonsComponent implements OnInit {
   public persons: any;
 
   public onePerson: any;
 
-  constructor(private _personsService: PersonsService, private _loginService: LoginService) {
-  }
+  public email = '';
+
+  constructor(
+    private _personsService: PersonsService,
+    private _loginService: LoginService
+  ) {}
 
   public ngOnInit(): void {
-    this._personsService.getPersons().subscribe((response: any) => (this.persons = response),
-      (error: any) => console.log(error));
+    this._personsService.getPersons().subscribe(
+      (response: any) => (this.persons = response),
+      (error: any) => console.log(error)
+    );
+  }
 
-    this._personsService.getPersonByEmail('juraj@admin.com').subscribe((response: any) => (this.onePerson = response),
-      (error: any) => console.log(error));
+  public getPersonByEmail(): void {
+    this._personsService.getPersonByEmail(this.email).subscribe(
+      (response: any) => (this.onePerson = response),
+      (error: any) => console.log(error)
+    );
   }
 
   public logout(): void {
     this._loginService.logout();
   }
-
-
 }
