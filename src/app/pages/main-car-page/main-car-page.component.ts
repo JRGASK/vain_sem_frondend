85324ec7c86b5955b42e4b61c14799c9db0cf2c5
jurajgajdiciar <juraj.gajdiciar9@gmail.com>
2025-01-13 22:40:03 +0100

    init buttons

--- src/app/pages/main-car-page/main-car-page.component.ts
+++ src/app/pages/main-car-page/main-car-page.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-main-car-page',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, CommonModule, HttpClientModule],
   templateUrl: './main-car-page.component.html',
   styleUrl: './main-car-page.component.css'
 })
 export class MainCarPageComponent {
+  constructor(
+    private _router: Router,
+  ) {
+  }
+
+  public toReservation():void {
+    this._router.navigate(['/createCustomerOrder']);
+  }
 
 }
--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, effect } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { IUser } from '../../user/IUser';
+import { Router, RouterOutlet } from '@angular/router';
+import { LoginService } from '../../auth/login.service';
+import { ErrorService } from '../../error/error.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [RouterOutlet, CommonModule, HttpClientModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
 
+  constructor(
+    private _router: Router,
+  ) {
+  }
+
+  public toCarsPage():void {
+    this._router.navigate(['/mainCarPage']);
+  }
 }
